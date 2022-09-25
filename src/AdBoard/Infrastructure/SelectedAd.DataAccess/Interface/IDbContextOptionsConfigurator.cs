8ea6431ae2f3f2bbab292ea6182fc798b2e77f6f
user <pos.monkira@gmail.com>
2022-09-25 20:17:16 +0300

    добавлен ConnectionString

---
+++ src/AdBoard/Infrastructure/SelectedAd.DataAccess/Interface/IDbContextOptionsConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectedAd.DataAccess.Interface
+{
+    /// <summary>
+    /// Конфигуратор контекста.
+    /// </summary>
+    public interface IDbContextOptionsConfigurator<TContext> where TContext : DbContext
+    {
+        /// <summary>
+        /// Выполняет конфигурацию контекста.
+        /// </summary>
+        /// <param name="opions">настройки</param>
+        void Configure(DbContextOptionsBuilder<TContext> opions);
+    }
+}
+ 
---
+++ src/AdBoard/Infrastructure/SelectedAd.DataAccess/SelectedAdContectConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SelectedAd.DataAccess.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectedAd.DataAccess
+{
+    public class SelectedAdContectConfiguration : IDbContextOptionsConfigurator<SelectedAdContext>
+    {
+        private const string PostgresConnectionStringName = "AdBoardDb";
+        private readonly IConfiguration _configuration;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public SelectedAdContectConfiguration( ILoggerFactory loggerFactory,IConfiguration configuration)
+        {
+            _loggerFactory = loggerFactory;
+            _configuration = configuration;
+        }
+
+
+        public SelectedAdContectConfiguration(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Configure(DbContextOptionsBuilder<SelectedAdContext> options)
+        {
+            var connectionString = _configuration.GetConnectionString(ConnectionStringName);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException(
+                    $"Не найдена строка подключения с именем '(ConnectionStringName)'");
+            }
+
+            options.UseNpgsql(connectionString)
+                .UseLoggerFactory(_loggerFactory);
+        }
+    }
+}
