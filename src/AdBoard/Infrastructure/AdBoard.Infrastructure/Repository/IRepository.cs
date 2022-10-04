488d7f9f57d9a1b7ef02c5832c672248ae865130
user <pos.monkira@gmail.com>
2022-10-04 16:24:34 +0300

    Добавлен репозиторий

--- src/AdBoard/Infrastructure/AdBoard.Infrastructure/Repository/IRepository.cs
+++ src/AdBoard/Infrastructure/AdBoard.Infrastructure/Repository/IRepository.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace AdBoard.Infrastructure.Repository
 {
+    /// <summary>
+    /// Базовый репозиторий
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface IRepository<TEntity> where TEntity : class
     {
+        ///<summary>
+        ///Получить все элменты сущности.
+        ///</summary>
+        /// <returns> Все элементы сущности <see cref="TEntity"/>.</returns>
+        IQueryable<TEntity> GetAll();
+
+        ///<summary>
+        ///Возвращает все значения по фильтру
+        ///</summary>
+        /// <param name="filter">Фильтр.</param>
+        ///<returns>Все элементы сущности <see cref="TEntity"/> по фильтру.</returns></returns>
+        IQueryable<TEntity> GetAllFiltered(Expression<Func<TEntity, bool>> filter);
+
+        ///<summary>
+        ///Возвращает все значения <see cref="TEntity"/> по Id
+        ///</summary>
+        ///<param name="Id"> Идентификатор <see cref="TEntity"/>.</param>
+        Task<TEntity> GetByIdAsync(Guid Id);
+
+        ///<summary>
+        ///Добавить элемент <see cref="TEntity"/>
+        ///</summary>
+        ///<param name="model"> Новая сущность <see cref="TEntity"/>.</param>
+        Task AddAsync(TEntity model);
+
+        /// <summary>
+        /// Обновляет элемент <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="model">Существующая сущность <see cref="TEntity"/>.</param>
+        Task UpdateAsync(TEntity model);
+
+        /// <summary>
+        /// Удаляет элемент <see cref="TEntity"/>.
+        /// </summary>
+        /// <param name="model">Существующая сущность <see cref="TEntity"/>.</param>
+        Task DeleteAsync(TEntity model);
     }
 }
+
---
+++ src/AdBoard/Infrastructure/AdBoard.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AdBoard.Infrastructure.Repository
+{
+    /// <inheritdoc />
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext DbContext { get; }
+
+        protected DbSet <TEntity> DbSet { get; }
+
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="Repository"/>.
+        /// </summary>
+        /// <param name="context">Контекст БД.</param>
+        public Repository(DbContext context)
+        {
+            DbContext = context;
+            DbSet = DbContext.Set<TEntity>();
+        }
+
+        /// <inheritdoc />
+        public async Task AddAsync(TEntity model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            await DbSet.AddAsync(model);
+            await DbContext.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task DeleteAsync(TEntity model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            DbSet.Remove(model);
+            await DbContext.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        public IQueryable<TEntity> GetAll()
+        {
+            return DbSet;
+        }
+
+        /// <inheritdoc />
+        public IQueryable<TEntity> GetAllFiltered(Expression<Func<TEntity, bool>> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+            return DbSet.Where(filter);
+        }
+
+        /// <inheritdoc />
+        public async Task<TEntity> GetByIdAsync(Guid Id)
+        {
+            return await DbSet.FindAsync(Id);
+        }
+
+        /// <inheritdoc />
+        public async Task UpdateAsync(TEntity model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            DbSet.Update(model);
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
+
